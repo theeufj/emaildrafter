595719f565520e016941b44fd8ba072995a6c605
theeufj <141145908+theeufj@users.noreply.github.com>
2024-10-17 19:37:17 +1100

    deploy script update

--- deployment.sh
+++ deployment.sh
@@ -2,5 +2,9 @@ git add -A
 git stash
 git pull
 go build .
-sudo systemctl daemon-reload && sudo systemctl enable goweb.service&& sudo systemctl start goweb.service
+echo "Waiting for 10 seconds for the app to build. ."
+sleep 10
+sudo systemctl daemon-reload && sudo systemctl enable goweb.service&& sudo systemctl stop goweb.service && sudo systemctl stop goweb.service
+echo "Waiting for 5 seconds for the app to build. ."
+sleep 5
 sudo journalctl -u goweb.service -f
